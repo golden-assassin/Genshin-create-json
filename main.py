5c4269aedb5165053e98c734cad3b75530b33901
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-29 19:19:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,38 +22,36 @@ def get_element_name(element_id):
 def dataSetup(UID=826487438,count=0,TYPE="攻撃力"):
   response = request(uid=UID)
   if response:
-    data, character, loc = response
-  result = artifact_Calculation(count=count,TYPE=TYPE,data=data,loc=loc)
-  chara = data['avatarInfoList'][count]
-  constellation = chara.get("talentIdList", [])
-  avatarId = data["playerInfo"]["showAvatarInfoList"][count]["avatarId"]
+    user, character, loc = response
+  result = artifact_Calculation(count=count,TYPE=TYPE,data=user,loc=loc)
+  user_character = user['avatarInfoList'][count]
+  constellation = user_character.get("talentIdList", [])
+  avatarId = user["playerInfo"]["showAvatarInfoList"][count]["avatarId"]
   element_name = get_element_name(character[str(avatarId)]["Element"])
 
   buf = 1
   fight_prop_keys = ["30", "40", "41", "42", "43", "44", "45", "46"]
   for key in fight_prop_keys:
-    if round(chara["fightPropMap"][key] * 100) > 0:
-      buf += round(chara["fightPropMap"][key] * 100,1) - 1
+    if round(user_character["fightPropMap"][key] * 100) > 0:
+      buf += round(user_character["fightPropMap"][key] * 100,1) - 1
       break
 
   talent = []
-  talents = chara["skillLevelMap"].values()
+  talents = user_character["skillLevelMap"].values()
   for tal in talents: talent.append(tal)
-  print(len(talents))
   if len(talents) > 3:
-    print(talent,talent[2])
     del talent[2]
 
-  if "proudSkillExtraLevelMap" in chara:
-    for idx, _ in enumerate(chara["proudSkillExtraLevelMap"].values(), start=1):
+  if "proudSkillExtraLevelMap" in user_character:
+    for idx, _ in enumerate(user_character["proudSkillExtraLevelMap"].values(), start=1):
       talent[idx] += 3
       if idx >= 2:
         break
   else:
     pass
 
-  weapon = chara["equipList"][5]["flat"]
-  parent_dict = chara["equipList"][5]["weapon"]["affixMap"]
+  weapon = user_character["equipList"][5]["flat"]
+  parent_dict = user_character["equipList"][5]["weapon"]["affixMap"]
   if parent_dict: _, weapon_rate = next(iter(parent_dict.items()))
   else: weapon_rate = 0
   weapon_rate += 1
@@ -63,8 +61,8 @@ def dataSetup(UID=826487438,count=0,TYPE="攻撃力"):
 
   output_json = {
     "uid": UID,
-    "name": data["playerInfo"]["nickname"],
-    "level": data["playerInfo"]["level"],
+    "name": user["playerInfo"]["nickname"],
+    "level": user["playerInfo"]["level"],
     "Character": {
       "UI": {
         "avatarId": avatarId,
@@ -73,16 +71,16 @@ def dataSetup(UID=826487438,count=0,TYPE="攻撃力"):
       },
       "Name": loc["ja"][str(character[str(avatarId)]["NameTextMapHash"])],
       "Const": len(constellation),
-      "Level": data["playerInfo"]["showAvatarInfoList"][count]["level"],
-      "Love": chara["fetterInfo"]["expLevel"],
+      "Level": user["playerInfo"]["showAvatarInfoList"][count]["level"],
+      "Love": user_character["fetterInfo"]["expLevel"],
       "Status": {
-        "HP": round(chara["fightPropMap"]["2000"],1),
-        "攻撃力": round(chara["fightPropMap"]["2001"],1),
-        "防御力": round(chara["fightPropMap"]["2002"],1),
-        "元素熟知": round(chara["fightPropMap"]["28"]),
-        "会心率": round(chara["fightPropMap"]["20"] * 100,1),
-        "会心ダメージ": round(chara["fightPropMap"]["22"] * 100,1),
-        "元素チャージ効率": round(chara["fightPropMap"]["23"] * 100,1),
+        "HP": round(user_character["fightPropMap"]["2000"],1),
+        "攻撃力": round(user_character["fightPropMap"]["2001"],1),
+        "防御力": round(user_character["fightPropMap"]["2002"],1),
+        "元素熟知": round(user_character["fightPropMap"]["28"]),
+        "会心率": round(user_character["fightPropMap"]["20"] * 100,1),
+        "会心ダメージ": round(user_character["fightPropMap"]["22"] * 100,1),
+        "元素チャージ効率": round(user_character["fightPropMap"]["23"] * 100,1),
         f"{element_name}元素ダメージ": round(buf,1)
       },
       "Talent": {
@@ -91,15 +89,15 @@ def dataSetup(UID=826487438,count=0,TYPE="攻撃力"):
         "爆発": talent[2],
       },
       "Base":{
-        "HP": round(chara["fightPropMap"]["1"],1),
-        "攻撃力": round(chara["fightPropMap"]["4"],1),
-        "防御力": round(chara["fightPropMap"]["7"],1)
+        "HP": round(user_character["fightPropMap"]["1"],1),
+        "攻撃力": round(user_character["fightPropMap"]["4"],1),
+        "防御力": round(user_character["fightPropMap"]["7"],1)
       },
     },
     "Weapon": {
       "UI_weapon": weapon["icon"] ,
       "name": loc["ja"][str(weapon["nameTextMapHash"])],
-      "Level": chara["equipList"][5]["weapon"]["level"],
+      "Level": user_character["equipList"][5]["weapon"]["level"],
       "totu": weapon_rate,
       "rarelity": weapon["rankLevel"],
       "BaseATK": weapon["weaponStats"][0]["statValue"],
