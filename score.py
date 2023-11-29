f2a7e1393effdda45104e75d9af954124d1ae620
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-29 21:42:56 +0900

    Update score.py

--- score.py
+++ score.py
@@ -1,6 +1,6 @@
 import json
 
-def artifact_Calculation(count=0,TYPE="攻撃力",data=None,loc=None):
+def artifact_Calculation(count=0,TYPE="攻撃力",user=None,loc=None):
   prop_to_japanese = {
     "FIGHT_PROP_HP_PERCENT": "HPパーセンテージ",
     "FIGHT_PROP_ATTACK_PERCENT": "攻撃パーセンテージ",
@@ -22,8 +22,8 @@ def artifact_Calculation(count=0,TYPE="攻撃力",data=None,loc=None):
     "FIGHT_PROP_ROCK_ADD_HURT": "岩元素ダメージ",
     "FIGHT_PROP_GRASS_ADD_HURT": "草元素ダメージ",
   }
-  if "avatarInfoList" in data and len(data["avatarInfoList"]) > 0:
-    first_avatar_info = data["avatarInfoList"][count]
+  if "avatarInfoList" in user and len(user["avatarInfoList"]) > 0:
+    first_avatar_info = user["avatarInfoList"][count]
     if "equipList" in first_avatar_info:
       Info = first_avatar_info["equipList"]
       if Info and len(Info) > 0:
@@ -33,30 +33,33 @@ def artifact_Calculation(count=0,TYPE="攻撃力",data=None,loc=None):
         for j in range(5):
           sublist = []
           for i in range(4):
-            artifact = Info[j]["flat"]["reliquarySubstats"][i]
-            statValue = float(artifact["statValue"])
-            converted_value = round(statValue, 1)
-            value = int(converted_value) if converted_value.is_integer() else converted_value
-            part = artifact["appendPropId"]
-            if part == "FIGHT_PROP_CRITICAL_HURT": score += value
-            if part == "FIGHT_PROP_CRITICAL": score += value * 2
-            if TYPE == "HP" and part == "FIGHT_PROP_HP_PERCENT": score += value
-            if TYPE == "攻撃力" and part == "FIGHT_PROP_ATTACK_PERCENT": score += value
-            if TYPE == "防御力" and part == "FIGHT_PROP_DEFENSE_PERCENT": score += value
-            if TYPE == "元素チャージ効率" and part == "FIGHT_PROP_CHARGE_EFFICIENCY": score += value
-            if TYPE == "元素熟知" and part == "FIGHT_PROP_ELEMENT_MASTERY": score += value * 0.25
-            if j == 0: flower += score
-            if j == 1: wing += score
-            if j == 2: clock += score
-            if j == 3: cup += score
-            if j == 4: crown += score
-            score = 0
-            reliquary = Info[j]["flat"]["reliquaryMainstat"]
-            sub_ja_name = prop_to_japanese.get(part, "")
-            sublist.append({
-              "option": sub_ja_name,
-              "value": value,
-            })
+            if j < len(Info) and i < len(Info[j]["flat"]["reliquarySubstats"]):
+              artifact = Info[j]["flat"]["reliquarySubstats"][i]
+              statValue = float(artifact["statValue"])
+              converted_value = round(statValue, 1)
+              value = int(converted_value) if converted_value.is_integer() else converted_value
+              part = artifact["appendPropId"]
+              if part == "FIGHT_PROP_CRITICAL_HURT": score += value
+              if part == "FIGHT_PROP_CRITICAL": score += value * 2
+              if TYPE == "HP" and part == "FIGHT_PROP_HP_PERCENT": score += value
+              if TYPE == "攻撃力" and part == "FIGHT_PROP_ATTACK_PERCENT": score += value
+              if TYPE == "防御力" and part == "FIGHT_PROP_DEFENSE_PERCENT": score += value
+              if TYPE == "元素チャージ効率" and part == "FIGHT_PROP_CHARGE_EFFICIENCY": score += value
+              if TYPE == "元素熟知" and part == "FIGHT_PROP_ELEMENT_MASTERY": score += value * 0.25
+              if j == 0: flower += score
+              if j == 1: wing += score
+              if j == 2: clock += score
+              if j == 3: cup += score
+              if j == 4: crown += score
+              score = 0
+              reliquary = Info[j]["flat"]["reliquaryMainstat"]
+              sub_ja_name = prop_to_japanese.get(part, "")
+              sublist.append({
+                "option": sub_ja_name,
+                "value": value,
+              })
+            else:
+              continue
           receipt.append(sublist)
           main_ja_name = prop_to_japanese.get(reliquary["mainPropId"], "")
           mainlist.append({"option":main_ja_name,"value":reliquary["statValue"]})
@@ -68,8 +71,8 @@ def artifact_Calculation(count=0,TYPE="攻撃力",data=None,loc=None):
             parts: {
               "icon": Info[i]["flat"]["icon"],
               "type": loc["ja"][str(artifactID)],
-              "Level": 20,
-              "rarelity": 5,
+              "Level":  Info[i]["reliquary"]["level"] - 1,
+              "rarelity": Info[i]["flat"]["rankLevel"],
               "main": mainlist[i],
               "sub": receipt[i]
             }
